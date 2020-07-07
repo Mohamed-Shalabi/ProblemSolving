502e3694a1b88c85785630d4dd645a418a02a7aa
Mohamed-Shalabi <48419551+mohamed-shalabi@users.noreply.github.com>
2020-07-07 22:03:09 +0200

    Problems solved in 8/7/2020.

---
+++ src/A/DieRoll.java
@@ -0,0 +1,30 @@
+package A;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * a class to solve the problem {@link = http://codeforces.com/contest/9/problem/A}
+ */
+public class DieRoll {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        char[] chars = bufferedReader.readLine().toCharArray();
+        int a = Character.getNumericValue(chars[0]);
+        int b = Character.getNumericValue(chars[2]);
+        int num = 7 - Math.max(a, b);
+        int gcd = 1;
+        for (int i = num; i > 1; i--) {
+            if (num % i == 0 && 6 % i == 0) {
+                gcd = i;
+                break;
+            }
+        }
+        b = 6 / gcd;
+        num = num / gcd;
+
+        System.out.println(num + "/" + b);
+    }
+
+}
---
+++ src/A/ShaassAndOskols.java
@@ -0,0 +1,42 @@
+package A;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * a class to solve the problem {@link = http://codeforces.com/contest/294/problem/A}
+ */
+public class ShaassAndOskols {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        bufferedReader.readLine();
+        String[] ais = bufferedReader.readLine().split(" ");
+        int[] aisInts = new int[ais.length];
+        int n = Integer.parseInt(bufferedReader.readLine());
+        int[] xs = new int[n];
+        int[] ys = new int[n];
+        for (int i = 0; i < n; i++) {
+            String[] strings = bufferedReader.readLine().split(" ");
+            xs[i] = Integer.parseInt(strings[0]);
+            ys[i] = Integer.parseInt(strings[1]);
+        }
+        for (int i = 0; i < ais.length; i++) {
+            aisInts[i] = Integer.parseInt(ais[i]);
+        }
+        for (int i = 0; i < n; i++) {
+            try {
+                aisInts[xs[i] - 2] += ys[i] - 1;
+            } catch (Exception ignored) {
+            }
+            try {
+                aisInts[xs[i]] += aisInts[xs[i] - 1] - ys[i];
+            } catch (Exception ignored) {
+            }
+            aisInts[xs[i] - 1] = 0;
+        }
+        for (int num : aisInts) {
+            System.out.printf("%d ", num);
+        }
+    }
+}
