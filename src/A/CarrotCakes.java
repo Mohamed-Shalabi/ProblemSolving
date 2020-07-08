cf29025ad3d0464082c82402c14212af90c7196a
Mohamed-Shalabi <48419551+mohamed-shalabi@users.noreply.github.com>
2020-07-08 23:40:02 +0200

    Problems solved in 8/7/2020.

--- src/A/CarrotCakes.java
+++ src/A/CarrotCakes.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+
 /**
  * a class to solve the problem {@link = http://codeforces.com/contest/799/problem/A}
  */
